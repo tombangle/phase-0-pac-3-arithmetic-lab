59c11a6bb9d1cbb1e46e88979fe97d00a2ef8f3c
Tom Bangle <tombangle@gmail.com>
2024-04-13 18:22:12 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+        return a + b;
+}
+
+function subtract(c, d){
+        return c - d;
+}
+
+function multiply(e, f){
+        return e * f;
+}
+
+function divide(g, h){
+        return g / h;
+}
+
+function increment(n) {
+        return (n + 1);
+}
+
+function decrement(m) {
+        return (m - 1)
+}
+
+function makeInt(a) {
+        return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+        return parseFloat(a, 10);
+}
